666f3bc9ec0d0c385c88b4123abde1577b0d47e4
soroush <soroush_safarii@yahoo.com>
2020-12-03 17:20:03 +0330

    day 3

--- d3/d3.py
+++ d3/d3.py
@@ -3,9 +3,12 @@ import operator
 
 with open('input.txt','r') as file :
 	data = file.readlines()
+	data = [i.rstrip("\n") for i in data]
 
-def find_trees(data):
-
+def find_trees_1(data):
+	"""
+		approach 1
+	"""
 	all_move_need = len(data) * 8
 	require_reapeat_pattern = (all_move_need // len(data[0]))
 
@@ -27,6 +30,22 @@ def find_trees(data):
 		trees_on_each_step.append(tree_count)
 	print(trees_on_each_step)
 
+def find_trees_2(data):
+	"""
+		approach 2
+	"""
+	slopes = [{"r":1,"d":1},{"r":3,"d":1},{"r":5,"d":1},{"r":7,"d":1},{"r":1,"d":2}]
+	trees_on_each_step = []
+	for slop in slopes:
+		down = 0
+		right = 0
+		tree = 0
+		while down+1 < len(data):
+			right += slop['r']
+			down += slop['d']
+			if data[down][right%len(data[down])] == '#' :
+				tree += 1
+		trees_on_each_step.append(tree)
+	print(trees_on_each_step)
 
-find_trees(data)
 
